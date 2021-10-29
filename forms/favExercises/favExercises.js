40c591946449886d0e47cab3e1f02fefa952e833
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-10-28 22:25:26 -0500

    Initial commit

---
+++ forms/favExercises/favExercises.js
@@ -0,0 +1,10 @@
+let exercises = ["pullups","pushups","situps","jogging","plank","curls"]
+let coreExercises = ["situps","plank"]
+
+favExercises.onshow=function(){
+
+}
+
+
+
+
---
+++ forms/mobileNav/mobileNav.js
@@ -0,0 +1,7 @@
+
+hmbMenu.onclick=function(s){
+ // add code to check if user clicked the hamburger control to open it 
+  /* add a switch here to take the user to the form
+     they chose. 
+  */
+}
